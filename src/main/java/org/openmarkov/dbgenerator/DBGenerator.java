6e7f92643eca6a96d62fe05bfedd9650436bc758
Jorge Pérez <jorgepmartin@gmail.com>
2018-03-21 13:32:56 +0100

    Applying Java (Eclipse) code format

--- src/main/java/org/openmarkov/dbgenerator/DBGenerator.java
+++ src/main/java/org/openmarkov/dbgenerator/DBGenerator.java
@@ -6,49 +6,45 @@
  */
 package org.openmarkov.dbgenerator;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
-
 import org.openmarkov.core.io.database.CaseDatabase;
 import org.openmarkov.core.model.network.Node;
 import org.openmarkov.core.model.network.ProbNet;
 import org.openmarkov.core.model.network.ProbNetOperations;
 import org.openmarkov.core.model.network.Variable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
 public class DBGenerator {
-	
+
 	/**
 	 * Generates a file containing a database of sampled cases
+	 *
 	 * @param probNet
 	 * @param numberOfCases
 	 */
-	public CaseDatabase generate(ProbNet probNet, int numberOfCases)
-	{
+	public CaseDatabase generate(ProbNet probNet, int numberOfCases) {
 		List<Node> nodes = probNet.getNodes();
 		int[][] cases = new int[numberOfCases][nodes.size()];
 		Random randomGenerator = new Random();
-		List<Node> sortedNodes = ProbNetOperations.sortTopologically (probNet);
+		List<Node> sortedNodes = ProbNetOperations.sortTopologically(probNet);
 		List<Integer> sortedNodeIndexes = new ArrayList<>();
-		for(Node node : sortedNodes)
-		{
+		for (Node node : sortedNodes) {
 			sortedNodeIndexes.add(nodes.indexOf(node));
 		}
-		for(int i=0; i <numberOfCases; ++i )
-		{
+		for (int i = 0; i < numberOfCases; ++i) {
 			HashMap<Variable, Integer> sampledStateIndexes = new HashMap<Variable, Integer>();
-			
-			for(int j=0; j < sortedNodeIndexes.size(); ++j)
-			{
+
+			for (int j = 0; j < sortedNodeIndexes.size(); ++j) {
 				Node node = sortedNodes.get(j);
 				int sampledIndex = node.getPotentials().get(0).sample(randomGenerator, sampledStateIndexes);
 				sampledStateIndexes.put(node.getVariable(), sampledIndex);
 				cases[i][sortedNodeIndexes.get(j)] = sampledIndex;
 			}
 		}
-		return new CaseDatabase(probNet.getVariables (), cases);
+		return new CaseDatabase(probNet.getVariables(), cases);
 	}
-	
 
 }
